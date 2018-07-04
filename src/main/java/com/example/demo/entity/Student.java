aff0fc0cb4c53fdc27bf4ef1f151b731abc95b45
ruangongdaolunkeshename <40746921+ruangongdaolunkeshename@users.noreply.github.com>
2018-07-04 12:01:53 +0800

    完善student实体类

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -15,4 +15,29 @@ public class Student {
     private String name;
     private Double score;
 
+    public Integer getid(){
+        return id;
+    }
+    public String no(){
+        return no;
+    }
+    public String name(){
+        return name;
+    }
+    public Double score(){
+        return score;
+    }
+    public void getid(Integer id){
+        this.id=id;
+    }
+    public void getno(String no){
+        this.no=no;
+    }
+    public void getname(String name){
+        this.name=name;
+    }
+    public void getscore(Double score){
+        this.score=score;
+    }
+
 }
