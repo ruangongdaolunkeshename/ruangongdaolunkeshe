8211c872dce39385f9e5e63f065adb058301a384
ruangongdaolunkeshename <40746921+ruangongdaolunkeshename@users.noreply.github.com>
2018-07-07 17:36:21 +0800

    增加新建文档功能，文档保存形式待定，文档修改方法待定

--- src/main/java/com/example/demo/service/impl/PaperServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/PaperServiceImpl.java
@@ -17,13 +17,10 @@ public class PaperServiceImpl implements PaperService {
         Student s=studentDAO.search(student.username());
         String oldpaper = studentDAO.getpapername(student.username());
         oldpaper = oldpaper + "|" + student.papername();
-        System.out.print(oldpaper+":");
         String oldupload = studentDAO.getuploadtime(student.username());
         oldupload = oldupload + "|" + student.uploadtime();
-        System.out.print(oldupload+":");
         String oldurl = studentDAO.getpaperurl(student.username());
         oldurl = oldurl + "|" + student.paperurl();
-        System.out.println(oldurl);
         s.setpapername(oldpaper);
         s.setuploadtime(oldupload);
         s.setpaperurl(oldurl);
